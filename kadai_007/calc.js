5b255b807e05dd9593136241386b303e607dff4a
chiyu-773 <chi712m@gmail.com>
2024-07-02 18:53:03 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,16 @@
+// 変数numに1以上の正の数を代入する
+const num = 7 ;
+
+// 変数numの値によって、処理を分ける
+if (num % 3 == 0) {
+    console.log("3の倍数です"); 
+}
+else if (num % 5 == 0) {
+    console.log("5の倍数です");
+}
+else if (num % 3 == 0 && num % 5 == 0) {
+    console.log("3と5の倍数です");
+}
+else {
+    console.log(num)
+}
